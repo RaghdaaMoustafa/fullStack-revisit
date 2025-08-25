d2bb6fa69f9d70a7c38c1570c96814b5f4e68aa6
RaghdaaMoustafa <Raghda.mustafa.anwar@gmail.com>
2025-08-25 20:04:22 +0300

    m

--- part3/index.js
+++ part3/index.js
@@ -29,7 +29,9 @@ let notes = [
 app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id
   const note = notes.find((note) => note.id === id)
